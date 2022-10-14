7bac34c18c1c92f67e25ed8ebf271c093d8cb771
natanbravo <NOBprogrammer8@gmail.com>
2022-10-14 19:38:30 -0300

    Feature
    Adição de laço de repetição : while

--- aula03/radar.py
+++ aula03/radar.py
@@ -1,17 +1,14 @@
+
 velocidade_permitida = 80
-velocidade_usuario = int(input('Sua velocidade é: '))
-
-if velocidade_usuario <= velocidade_permitida:
-    print('Não houve multa')
-elif velocidade_usuario > velocidade_permitida and velocidade_usuario <= velocidade_permitida + 10:
-    print('Levou multa leve')
-elif velocidade_usuario >= velocidade_permitida + 11 and velocidade_usuario <= velocidade_permitida + 20:
-    print('Levou multa grave')
-elif velocidade_usuario > velocidade_permitida + 20:
-    print('Levou multa gravíssima')
 
 
-
-
-
-
+while velocidade_permitida:
+    velocidade_usuario = int(input('Sua velocidade é: '))
+    if velocidade_usuario <= velocidade_permitida:
+        print('Não houve multa')
+    elif velocidade_usuario > velocidade_permitida and velocidade_usuario <= velocidade_permitida + 10:
+        print('Levou multa leve')
+    elif velocidade_usuario >= velocidade_permitida + 11 and velocidade_usuario <= velocidade_permitida + 20:
+        print('Levou multa grave')
+    elif velocidade_usuario > velocidade_permitida + 20:
+        print('Levou multa gravíssima')
